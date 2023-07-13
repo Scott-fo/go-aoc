f7d84e03a95c360e77bb8b29b32548a6ba501389
Scott-fo <scottfo@live.com>
2023-07-13 22:09:25 +0100

    feat: day2

---
+++ 2022/day2/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type Move string
+
+const (
+	Rock     Move = "rock"
+	Paper    Move = "paper"
+	Scissors Move = "scissors"
+)
+
+type Result string
+
+const (
+	Win  Result = "win"
+	Lose Result = "lose"
+	Draw Result = "draw"
+)
+
+var winMap = map[Move]Move{
+	Rock:     Paper,
+	Paper:    Scissors,
+	Scissors: Rock,
+}
+
+var loseMap = map[Move]Move{
+	Rock:     Scissors,
+	Paper:    Rock,
+	Scissors: Paper,
+}
+
+func parseMove(s string) (Move, error) {
+	switch s {
+	case "A", "X":
+		return Rock, nil
+	case "B", "Y":
+		return Paper, nil
+	case "C", "Z":
+		return Scissors, nil
+	}
+
+	return "", errors.New("invalid move")
+}
+
+func parseStrategy(s string) (Result, error) {
+	switch s {
+	case "X":
+		return Lose, nil
+	case "Y":
+		return Draw, nil
+	case "Z":
+		return Win, nil
+	}
+
+	return "", errors.New("unexpected strategy")
+}
+
+func getMoveScore(m Move) (int, error) {
+	switch m {
+	case Rock:
+		return 1, nil
+	case Paper:
+		return 2, nil
+	case Scissors:
+		return 3, nil
+	}
+
+	return 0, errors.New("invalid move")
+}
+
+func getResultScore(r Result) (int, error) {
+	switch r {
+	case Win:
+		return 6, nil
+	case Lose:
+		return 0, nil
+	case Draw:
+		return 3, nil
+	}
+
+	return 0, errors.New("invalid result")
+}
+
+func getResult(y, o Move) Result {
+	switch {
+	case y == o:
+		return Draw
+	case (y == Rock && o == Scissors) || (y == Paper && o == Rock) || (y == Scissors && o == Paper):
+		return Win
+	default:
+		return Lose
+	}
+}
+
+func getMoveToPlay(o Move, r Result) (Move, error) {
+	if r == Draw {
+		return o, nil
+	}
+
+	if r == Win {
+		return winMap[o], nil
+	}
+
+	if r == Lose {
+		return loseMap[o], nil
+	}
+
+	return "", errors.New("unexpected strategy")
+
+}
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal("Failed to open input")
+	}
+
+	defer file.Close()
+
+	score := 0
+
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		r := strings.Split(s.Text(), " ")
+
+		o, err := parseMove(r[0])
+		if err != nil {
+			log.Fatal("Unexpected move")
+		}
+
+		st, err := parseStrategy(r[1])
+		if err != nil {
+			log.Fatal("Unexpected strategy")
+		}
+
+		m, err := getMoveToPlay(o, st)
+		if err != nil {
+			log.Fatal("Unexpected strategy")
+		}
+
+		rs, err := getResultScore(st)
+		if err != nil {
+			log.Fatal("Unexpected result")
+		}
+
+		ms, err := getMoveScore(m)
+		if err != nil {
+			log.Fatal("Unexpected move")
+		}
+
+		score += rs + ms
+	}
+
+	fmt.Println("Score: ", score)
+}
