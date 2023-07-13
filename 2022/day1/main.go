934ec0038f7522369249621b2d1fefc904f20293
Scott-fo <scottfo@live.com>
2023-07-13 18:38:30 +0100

    feat: heap impl

--- 2022/day1/main.go
+++ 2022/day1/main.go
@@ -3,12 +3,31 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"container/heap"
 	"fmt"
 	"log"
+	"math"
 	"os"
-	"sort"
 	"strconv"
 )
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	heap := *h
+	length := len(heap)
+	out := heap[length-1]
+	*h = heap[0 : length-1]
+	return out
+}
 
 func main() {
 	file, err := os.Open("./input.txt")
@@ -18,14 +37,21 @@ func main() {
 
 	defer file.Close()
 
-	var elfMap []int
+	h := &IntHeap{}
+	heap.Init(h)
+
 	elfVal := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := bytes.TrimSpace(scanner.Bytes())
 		if len(line) == 0 {
-			elfMap = append(elfMap, elfVal)
+			if h.Len() < 3 {
+				heap.Push(h, elfVal)
+			} else if elfVal > (*h)[0] {
+				heap.Pop(h)
+				heap.Push(h, elfVal)
+			}
 			elfVal = 0
 			continue
 		}
@@ -38,23 +64,27 @@ func main() {
 		elfVal += val
 	}
 
-	if elfVal > 0 {
-		elfMap = append(elfMap, elfVal)
+	if h.Len() < 3 {
+		heap.Push(h, elfVal)
+	} else if elfVal > (*h)[0] {
+		heap.Pop(h)
+		heap.Push(h, elfVal)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	sort.Slice(elfMap, func(i, j int) bool {
-		return elfMap[i] > elfMap[j]
-	})
-
-	fmt.Println(elfMap[0])
 	sum := 0
-	for _, val := range elfMap[0:3] {
-		sum += val
+	max := math.MinInt
+	for h.Len() > 0 {
+		curr := heap.Pop(h).(int)
+		if curr > max {
+			max = curr
+		}
+		sum += curr
 	}
 
+	fmt.Println(max)
 	fmt.Println(sum)
 }
